105af0dafacba4ffc1809e09e0132f2c5e4f73c6
raihanfhs <jonkyinhere@gmail.com>
2025-07-02 04:10:52 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,25 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 
 // Import Chart.js and necessary components
-import { Chart, BarController, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import {
+  Chart,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+  Tooltip,
+  Legend,
+  BarController, 
+  BarElement, 
+  ArcElement, 
+  PieController
+} from 'chart.js';
 
 // Register the controllers and elements you will use
-Chart.register(BarController, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+Chart.register(BarController, LineController, LineElement, PointElement,CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement, PieController);
+
 
 window.Alpine = Alpine;
 window.Chart = Chart; // Make Chart.js globally accessible if needed in other scripts or inline
